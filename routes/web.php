44cadf7c2962aa0b603310fef0b416fcd9cb1954
Athriya Genferin <2241720075@student.polinema.ac.id>
2024-02-23 15:16:59 +0700

    routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,45 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//Practicum 1
+Route::get('/hello', function () { 
+    return 'Hello World'; 
+    }); 
+
+Route::get('/world', function () { 
+    return 'World'; 
+    }); 
+
+Route::get('/welcome', function () { 
+    return 'Welcome'; 
+    }); 
+
+Route::get('/namanim', function () { 
+    return 'Name: Athriya Genferin <br> NIM: 2241720075'; 
+    }); 
+
+//Practicum 2
+Route::get('/user/{name}', function ($name) { 
+    return 'My name is '.$name; 
+    }); 
+
+Route::get('/posts/{post}/comments/{comment}', function 
+    ($postId, $commentId) { 
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId; 
+    }); 
+
+Route::get('/articles/{id}', function 
+    ($articleId) { 
+    return 'Article page-'.$articleId; 
+    }); 
+
+//practicum 3
+Route::get('/user/{name?}', function ($name=null) { 
+    return 'My name is '.$name; 
+    });
+
+Route::get('/user/{name?}', function ($name='John') { 
+        return 'Nama saya '.$name; 
+        });
